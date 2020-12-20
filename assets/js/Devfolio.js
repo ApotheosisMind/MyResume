262f0ad51f033145730c28eb7c465c64638c1bb5
Apotheosis Mind <64317865+ApotheosisMind@users.noreply.github.com>
2020-12-20 17:29:53 +0530

    update

---
+++ assets/js/Devfolio.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let devfolioOptions = {
+        buttonSelector: '#devfolio-apply-now',
+        key: 'thepythonweek',
+    }
+
+    let script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co';
+    script.async = true;
+    document.head.append(script);
+
+    script.onload = function() {
+        new Devfolio(devfolioOptions);
+    }
+
+
+
+    script.onerror = function() {
+        document.querySelector(devfolioOptions.buttonSelector).addEventListener('click', function() {
+            window.location.href = 'https://devfolio.co/external-apply/' + devfolioOptions.key;
+        });
+    }
+});
